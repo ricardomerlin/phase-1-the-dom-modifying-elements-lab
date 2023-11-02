1dfdfa5bb7433cd94740d843ad99752ba3dbffd9
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-11-01 20:39:09 -0400

    uploading dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+let main = document.getElementById("main")
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = "Ricardo is the champion"
+document.body.append(newHeader)
+
+
